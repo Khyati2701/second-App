47def6f9577afa7c66d273832c7f3d4f842b5895
Khyati2701 <124777242+Khyati2701@users.noreply.github.com>
2023-04-10 15:27:53 +0530

    conditional rendring

--- src/Component/Class-Component/06ConditionalRenderingInClassCompo.jsx
+++ src/Component/Class-Component/06ConditionalRenderingInClassCompo.jsx
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
-class ConditionalRenderingInClassCompo extends Component{
-    constructor(props){
+class ConditionalRenderingInClassCompo extends Component {
+    constructor(props) {
         super(props);
         this.state={
             status:true
         }
     }
-    loginChange = () => {
+    
+    loginChange=()=>{
         this.setState({status:!this.state.status})
     }
-    render(){
+    render() {
         let loginbtn = ""
         const count = 1;
-        return(
-            <>
-            {count && <h1>Messages: {count}</h1>}
-            <br />
-            {loginbtn}
-            <br />
-            <button className="btn btn-primary" onClick={this.loginChange}>
-                {(this.state.status) ? "logout" :"login" }
-                </button>
-                <br />
-                <br />
-                condition ? true : false.
-                <br />
-                <br />
-                <button className="btn btn-info" onClick={() => {this.setState({status:!this.state.status})}}>{(this.state.status)?"Logout":"Login"}</button>
-            </>
-        );
-    }
-}
+
+            return (
+                <>
+                      {count && <h1>Messages: {count}</h1>}
+
+                    <br />
+                    {loginbtn}    
+                    <br />
+                    <button className='btn btn-primary' onClick={this.loginChange}> 
+                        { (this.state.status) ? "Logout" :"login" }
+                    </button>
+                    <br />
+                    <br />
+                    condition ? true : false.
+                    <br />
+                    <br />
+                    <button className='btn btn-info' onClick={()=>{ this.setState({status:!this.state.status})}}> { (this.state.status)? "Logout":"Login" }</button>
+                </>
+            );
+    };
+};
+
+export default ConditionalRenderingInClassCompo;
