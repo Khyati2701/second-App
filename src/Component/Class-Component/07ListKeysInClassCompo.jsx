3c225a442bdf881d7200d2be9d5a0311c1ba6a2c
Khyati2701 <124777242+Khyati2701@users.noreply.github.com>
2023-04-17 14:21:11 +0530

    list and keys

--- src/Component/Class-Component/07ListKeysInClassCompo.jsx
+++ src/Component/Class-Component/07ListKeysInClassCompo.jsx
@@ -8,12 +8,33 @@ class ListKeysInClassCompo extends Component{
         //array: it is non primitive datatype.it can use to store  multipal datatype in one datatype.
         //array: return index start with(0).
         //object: return key and value.
+        //map:callback function 6.
+        //ek j array ne destructure kar vano hoy. 
+        //keys: unique identification of list.
+        //typecasting: ek datatype ne bija ma  change karvu.
+        //method means predefine function.
+        constructor(){
+            super()
+            this.state = {
+            items: [1,2,3,4,5]
+            }
+        }
+        update = () =>{
+            this.setState({
+                items: [1,2,3,4,5,6]
+            })
+        }
     render(){
         const items = [1,2,3,4,5]
+        const mylist = this.state.items.map((mydata)=> <li key={mydata.toString()}>{mydata}</li>)
         console.log(items);
         return(
             <>
-            <h1>return</h1>
+            <h1>this is my list and keys</h1>
+            <div>
+                <ul>{mylist}</ul>
+                <button onClick={this.update}>update</button>
+            </div>
             </>
         )
     }
